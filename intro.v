7c33f853effd16294873901a603c147dcfd58a33
zjhmale <zjhmale@gmail.com>
2016-05-20 11:59:25 +0800

    finish intro

---
+++ intro.v
@@ -0,0 +1,32 @@
+(* Basic Propositions *)
+
+Variables P Q R : Prop.
+
+(* simple tautology proof *)
+
+Theorem I : P -> P.
+
+Proof.
+  intro H.                      (* to prove P -> Q, assume P and prove Q *)
+  exact H.                      (* current goal is just the assumption *)
+Qed.
+
+Theorem K : P -> (Q -> P).
+
+Proof.
+  intro H1.
+  intro H2.
+  exact H1.
+Qed.
+
+Theorem S : (P -> Q -> R) -> (P -> Q) -> P -> R.
+
+Proof.
+  intro pqrH.
+  intro pqH.
+  intro pH.
+  apply pqrH.                   (* MP *)
+  exact pH.
+  apply pqH.                    (* MP *)
+  exact pH.
+Qed.

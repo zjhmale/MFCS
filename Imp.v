6b9675edffa8c682476cb16907b795c99e13d79f
zjhmale <zjhmale@gmail.com>
2016-08-24 14:23:44 +0800

    imp syntax

---
+++ Imp.v
@@ -0,0 +1,20 @@
+Require Export SfLib.
+
+(* syntax *)
+Module AExp.
+
+  Inductive aexp : Type :=
+  | ANum : nat -> aexp
+  | APlus : aexp -> aexp -> aexp
+  | AMinus : aexp -> aexp -> aexp
+  | AMult : aexp -> aexp -> aexp.
+
+  Inductive bexp : Type :=
+  | BTrue : bexp
+  | BFalse : bexp
+  | BEq : aexp -> aexp -> bexp
+  | BLe : aexp -> aexp -> bexp
+  | BNot : bexp -> bexp
+  | BAnd : bexp -> bexp -> bexp.
+
+End AExp.
